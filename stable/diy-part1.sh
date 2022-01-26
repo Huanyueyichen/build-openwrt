99253f89aab22c46a1d4ef33359ac4f9cc831f3d
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2022-01-26 16:49:09 +0800

    Create diy-part1.sh

---
+++ stable/diy-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+
+
+
+## x86机型,默认内核5.10，修改内核为5.15
+# sed -i 's/PATCHVER:=5.10/PATCHVER:=5.15/g' target/linux/x86/Makefile
