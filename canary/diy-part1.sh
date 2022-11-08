a2f500c4add77c94b2c9aefb7b5ba3cf5e17ceab
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2022-11-08 19:14:30 +0800

    Update diy-part1.sh

--- canary/diy-part1.sh
+++ canary/diy-part1.sh
@@ -13,5 +13,5 @@
 
 
 
-## x86机型,默认内核5.10，修改内核为5.15
-sed -i 's/PATCHVER:=5.15/PATCHVER:=5.19/g' target/linux/x86/Makefile
+## x86机型,默认内核5.15，修改内核为6.0
+sed -i 's/PATCHVER:=5.15/PATCHVER:=6.0/g' target/linux/x86/Makefile
