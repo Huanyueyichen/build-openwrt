ced8f3887f7cb7654460be3d5fb05a1decb1e34e
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2022-11-13 18:01:33 +0800

    Update diy-part2.sh

--- stable/diy-part2.sh
+++ stable/diy-part2.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #sed -i "s/OpenWrt /星新课代表/g" package/lean/default-settings/files/zzz-default-settings
 
 #删除原默认主题
-rm -rf /feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/small8/luci-theme-argon
 rm -rf feeds/small8/luci-app-argon-config
 #rm -rf feeds/luci/luci-theme-bootstrap
