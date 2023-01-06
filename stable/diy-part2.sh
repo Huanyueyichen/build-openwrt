27dbf43dbab8a52bb8627a9ec7fef5cc252238d6
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-01-06 11:07:58 +0800

    Update diy-part2.sh

--- stable/diy-part2.sh
+++ stable/diy-part2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt修改你喜欢的就行（不能纯数字或者使用中文）
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
