e7714aa8882f68217dc3b460dd2c4d99b34c2890
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-02-19 16:52:16 +0800

    Update diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -13,7 +13,9 @@
 
 
 
-## x86机型,默认内核5.15，修改内核为5.15
-#sed -i 's/PATCHVER:=5.15/PATCHVER:=5.15/g' target/linux/x86/Makefile
+## x86机型,默认内核5.15，修改内核为6.1
+sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 
+## arm机型,默认内核5.15，修改内核为6.1
+sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/rockchip/Makefile
