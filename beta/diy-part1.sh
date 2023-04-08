bfaab23470d397353eb8d864523dae73accecdf6
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-04-08 10:54:02 +0800

    Update diy-part1.sh

--- beta/diy-part1.sh
+++ beta/diy-part1.sh
@@ -19,3 +19,7 @@ sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 ## arm机型,默认内核5.15，修改内核为6.1
 sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/rockchip/Makefile
+
+## r2c修复wan口
+rm -rf target/linux/rockchip/patches-6.1/204-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch
+cp -f $GITHUB_WORKSPACE/patches/204-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch target/linux/rockchip/patches-6.1/204-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch
