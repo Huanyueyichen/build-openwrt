95e321c08e2a00eccf341b229f6619286c5e47d1
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-06-13 08:53:04 +0800

    Update diy-part1.sh

--- stable/diy-part1.sh
+++ stable/diy-part1.sh
@@ -29,6 +29,11 @@
 #rm -rf target/linux/rockchip/patches-5.15/204-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch
 #cp -f $GITHUB_WORKSPACE/patches/204-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch target/linux/rockchip/patches-5.15/204-rockchip-rk3328-Add-support-for-FriendlyARM-NanoPi-R.patch
 
+## r2s r2c风扇脚本
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
+wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan
+wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh
+
 
 ## 修改R4A千兆版闪存布局为Breed直刷版
 # 1.修改 mt7621_xiaomi_mir3g-v2.dts
