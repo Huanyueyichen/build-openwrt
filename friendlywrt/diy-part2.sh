17c775f56195a5245e7514eab7b9ae02cded76c2
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-06-17 11:14:34 +0800

    Update diy-part2.sh

--- friendlywrt/diy-part2.sh
+++ friendlywrt/diy-part2.sh
@@ -3,3 +3,10 @@
 
 # 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+
+
+#取消原主题luci-theme-bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
