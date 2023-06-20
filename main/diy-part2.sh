818f567def4ce681e291176f31e0c214d934a5d8
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-06-20 17:41:51 +0800

    Update diy-part2.sh

--- main/diy-part2.sh
+++ main/diy-part2.sh
@@ -18,3 +18,52 @@ wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/fr
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v22.03/target/linux/rockchip/armv8/base-files/usr/bin/cputemp.sh
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v22.03/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v22.03/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh
+
+## alist
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+
+## Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+
+## autoreboot
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-autoreboot package/luci-app-autoreboot
+
+## Add luci-app-poweroff
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-poweroff package/luci-app-poweroff
+
+## Add luci-app-wolplus
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wolplus package/luci-app-wolplus
+
+## Add luci-app-onliner (need luci-app-nlbwmon)
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-onliner package/luci-app-onliner
+
+## Add luci-app-irqbalance by QiuSimons https://github.com/QiuSimons/OpenWrt-Add
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-irqbalance package/luci-app-irqbalance
+
+## AdguardHome
+svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+
+## luci-app-lucky
+svn export https://github.com/kiddin9/openwrt-packages/trunk/lucky package/lucky
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-lucky package/luci-app-lucky
+
+## qbittorrent
+svn export https://github.com/kiddin9/openwrt-packages/trunk/qBittorrent-Enhanced-Edition package/qBittorrent-Enhanced-Edition
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-qbittorrent package/luci-app-qbittorrent
+
+## file
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filetransfer package/luci-app-filetransfer
+
+## cpufreq
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-cpufreq package/luci-app-cpufreq
+
+## wrtbwmon
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
+svn export https://github.com/kiddin9/openwrt-packages/trunk/wrtbwmon package/wrtbwmon
+
+## ramfree
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ramfree package/luci-app-ramfree
