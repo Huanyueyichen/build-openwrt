1cbc7ff6e2da274da39df1eb161fb10a6c29732f
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-06-20 19:45:40 +0800

    Update diy-part1.sh

--- main/diy-part1.sh
+++ main/diy-part1.sh
@@ -26,5 +26,6 @@ wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/frien
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v22.03/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v22.03/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh
 
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+## Alist支持
+# rm -rf feeds/packages/lang/golang
+# svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
