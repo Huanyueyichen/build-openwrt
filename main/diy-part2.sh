b6f86c86f5eb543882ebfb96d67d8ec396dfaa8d
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-06-21 14:50:45 +0800

    Update diy-part2.sh

--- main/diy-part2.sh
+++ main/diy-part2.sh
@@ -86,8 +86,22 @@ svn export https://github.com/kiddin9/openwrt-packages/trunk/ddns-scripts packag
 ## fileassistant依赖
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-lib-fs package/new/luci-lib-fs
 
-## ssr passwall vssr bypass依赖
-git clone https://github.com/kenzok8/small package/new/small
+## ssr-plus依赖
+svn export https://github.com/kiddin9/openwrt-packages/trunk/dns2socks package/new/dns2socks
+svn export https://github.com/kiddin9/openwrt-packages/trunk/dns2tcp package/new/dns2tcp
+svn export https://github.com/kiddin9/openwrt-packages/trunk/lua-neturl package/new/lua-neturl
+svn export https://github.com/kiddin9/openwrt-packages/trunk/tcping package/new/tcping
+svn export https://github.com/kiddin9/openwrt-packages/trunk/shadowsocksr-libev package/new/shadowsocksr-libev
+svn export https://github.com/kiddin9/openwrt-packages/trunk/chinadns-ng package/new/chinadns-ng
+svn export https://github.com/kiddin9/openwrt-packages/trunk/hysteria package/new/hysteria
+svn export https://github.com/kiddin9/openwrt-packages/trunk/ipt2socks package/new/ipt2socks
+svn export https://github.com/kiddin9/openwrt-packages/trunk/naiveproxy package/new/naiveproxy
+svn export https://github.com/kiddin9/openwrt-packages/trunk/redsocks2 package/new/redsocks2
+svn export https://github.com/kiddin9/openwrt-packages/trunk/shadowsocks-rust package/new/shadowsocks-rust
+svn export https://github.com/kiddin9/openwrt-packages/trunk/simple-obfs package/new/simple-obfs
+svn export https://github.com/kiddin9/openwrt-packages/trunk/v2ray-plugin package/new/v2ray-plugin
+svn export https://github.com/kiddin9/openwrt-packages/trunk/trojan package/new/trojan
+svn export https://github.com/kiddin9/openwrt-packages/trunk/gn package/new/gn
 
 # svn export https://github.com/kiddin9/openwrt-packages/trunk/qt6tools package/qt6tools
 # svn export https://github.com/kiddin9/openwrt-packages/trunk/qt6base package/qt6base
