83e1c07597eed839e382ba068ee51b89fb1b8a19
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-06-23 10:17:11 +0800

    Create start-rk3328-pwm-fan.sh

---
+++ patches/start-rk3328-pwm-fan.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ ! -d /sys/class/pwm/pwmchip0 ]; then
+    echo "this model does not support pwm."
+    exit 1
+fi
+
+if [ ! -d /sys/class/pwm/pwmchip0/pwm0 ]; then
+    echo -n 0 > /sys/class/pwm/pwmchip0/export
+fi
+sleep 1
+while [ ! -d /sys/class/pwm/pwmchip0/pwm0 ];
+do
+    sleep 1
+done
+ISENABLE=`cat /sys/class/pwm/pwmchip0/pwm0/enable`
+if [ $ISENABLE -eq 1 ]; then
+    echo -n 0 > /sys/class/pwm/pwmchip0/pwm0/enable
+fi
+echo -n 50000 > /sys/class/pwm/pwmchip0/pwm0/period
+echo -n 1 > /sys/class/pwm/pwmchip0/pwm0/enable
+
+echo -n 49990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+
+while true
+do
+        temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        if  [ $temp -gt 50000 ]; then
+                echo 30000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
+        elif  [ $temp -le 40000 ]; then
+                echo 49990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
+        fi
+        sleep 60s;
+done
