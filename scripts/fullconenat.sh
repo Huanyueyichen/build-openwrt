66c991e6d0328962ce9b271ff303b59a0a1ed10d
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-07-21 06:58:24 +0800

    Create fullconenat.sh

---
+++ scripts/fullconenat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download kernel patch to target/linux/generic/hack-5.4/
+wget -P target/linux/generic/hack-5.4/ https://github.com/Paull/openwrt-fullconenat/raw/master/files/952-net-conntrack-events-support-multiple-registrant.patch
+
+# Download firewall3 patch to package/network/config/firewall/patches/
+mkdir package/network/config/firewall/patches
+wget -P package/network/config/firewall/patches/ https://github.com/Paull/openwrt-fullconenat/raw/master/files/fullconenat-fw3.patch
+
+# Clone this repo
+git clone -b master --single-branch https://github.com/Paull/openwrt-fullconenat package/new/fullconenat
