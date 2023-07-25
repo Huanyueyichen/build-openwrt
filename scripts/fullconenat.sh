f60948fa7a251cb701c12965a1c6b694df2a41fc
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-07-25 16:50:34 +0800

    Update fullconenat.sh

--- scripts/fullconenat.sh
+++ scripts/fullconenat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn export https://github.com/immortalwrt/immortalwrt/tree/openwrt-21.02/package/network/utils/fullconenat package/new/fullconenat
+svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/network/utils/fullconenat package/new/fullconenat
 wget -P target/linux/generic/hack-5.4/ https://github.com/immortalwrt/immortalwrt/blob/openwrt-21.02/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
 patch -d feeds/luci -p1 -i $GITHUB_WORKSPACE/patches/fullconenat-luci.patch
-svn export https://github.com/immortalwrt/immortalwrt/tree/openwrt-21.02/package/network/config/firewall/patches package/network/config/firewall/patches
+svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/network/config/firewall/patches package/network/config/firewall/patches
