d63ad08b00657766b91ef2799bed9686ac50ecd7
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-07-29 07:44:53 +0800

    Update and rename diyr4a2305.sh to diy-r4a-2305.sh

--- main/diy-r4a-2305.sh
+++ main/diy-r4a-2305.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-## rockchip机型,默认内核5.15，修改内核为6.1
-# sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/rockchip/Makefile
+## 默认开启wifi
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 ## 移除 SNAPSHOT 标签
-# sed -i 's,-SNAPSHOT,,g' include/version.mk
-# sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
+sed -i 's,-SNAPSHOT,,g' include/version.mk
+sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 
 ## 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
-# sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-
-## alist编译环境
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
-
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 
 rm -rf package/new
@@ -55,19 +50,5 @@ svn export https://github.com/kiddin9/openwrt-packages/trunk/autocore package/ne
 ## Add luci-app-wireguard
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wireguard package/new/luci-app-wireguard
 
-
-## openclash
-# bash $GITHUB_WORKSPACE/scripts/openclash.sh arm64
-
-## luci-app-ssr-plus
-# bash $GITHUB_WORKSPACE/scripts/ssrp.sh
-
-## ShellClash
-# bash $GITHUB_WORKSPACE/scripts/ShellClash.sh
-
-## zsh
-# bash $GITHUB_WORKSPACE/scripts/zsh.sh
-
-## turboacc_6_1
-# sh ~/turboacc_6_1.sh
-sh ~/Documents/turboacc_5_15.sh
+## turboacc_5_15
+bash $GITHUB_WORKSPACE/scripts/turboacc_5_15.sh
