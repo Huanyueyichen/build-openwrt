700787f78f4677266b4b0b9685944b3203bee405
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-07-29 14:51:33 +0800

    Update diy-r4a.sh

--- lede/diy-r4a.sh
+++ lede/diy-r4a.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## ramips机型,默认内核5.10，修改内核为5.10
+sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/ramips/Makefile
 
 ## 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
