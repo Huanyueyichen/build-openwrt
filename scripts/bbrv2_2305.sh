2602e6b551307763e4b09d19ccd9934acf22cd6b
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-08-05 09:39:35 +0800

    Update bbrv2_2305.sh

--- scripts/bbrv2_2305.sh
+++ scripts/bbrv2_2305.sh
@@ -2,5 +2,5 @@
 
 cp -rf $GITHUB_WORKSPACE/patches/BBRv2/kernel/* ./target/linux/generic/hack-5.15/
 cp -rf $GITHUB_WORKSPACE/patches/BBRv2/kernel/* ./target/linux/generic/hack-6.1/
-cp -rf $GITHUB_WORKSPACE/patches/BBRv2/openwrt/package ./
+cp -rf $GITHUB_WORKSPACE/patches/BBRv2/openwrt/package/kernel/linux/files/sysctl-tcp-bbr2.conf package/kernel/linux/files/sysctl-tcp-bbr2.conf
 wget -qO - https://github.com/openwrt/openwrt/commit/7db9763.patch | patch -p1
