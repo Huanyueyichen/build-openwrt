a760a7dc37f3eb4ead4593888177893b57e6e42c
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-08-05 08:16:50 +0800

    Update bbrv2.sh

--- scripts/bbrv2.sh
+++ scripts/bbrv2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp -rf ../patches/BBRv2/kernel/* ./target/linux/generic/hack-5.15/
-cp -rf ../patches/BBRv2/kernel/* ./target/linux/generic/hack-5.10/
-cp -rf ../patches/BBRv2/kernel/* ./target/linux/generic/hack-6.1/
-cp -rf ../patches/BBRv2/openwrt/package ./
+cp -rf $GITHUB_WORKSPACE/patches/BBRv2/kernel/* ./target/linux/generic/hack-5.15/
+cp -rf $GITHUB_WORKSPACE/patches/BBRv2/kernel/* ./target/linux/generic/hack-5.10/
+cp -rf $GITHUB_WORKSPACE/patches/BBRv2/kernel/* ./target/linux/generic/hack-6.1/
+cp -rf $GITHUB_WORKSPACE/patches/BBRv2/openwrt/package ./
 wget -qO - https://github.com/openwrt/openwrt/commit/7db9763.patch | patch -p1
