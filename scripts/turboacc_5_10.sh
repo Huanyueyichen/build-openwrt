af8aaacaa32e12565f638ef8851d963e88ab2804
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-08-07 10:21:50 +0800

    Update turboacc_5_10.sh

--- scripts/turboacc_5_10.sh
+++ scripts/turboacc_5_10.sh
@@ -11,7 +11,7 @@ rm -rf ./turboacc
 cd ../..
 cp -f turboacc_tmp/turboacc/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
 cp -f turboacc_tmp/turboacc/hack-5.10/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-5.10/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
-cp -f turboacc_tmp/turboacc/pending-5.10/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-5.10/613-netfilter_optional_tcp_window_check.patch
+cp -f turboacc_tmp/turboacc/pending-5.10/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/hack-5.10/613-netfilter_optional_tcp_window_check.patch
 rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
 mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
 cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
@@ -21,3 +21,5 @@ cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboa
 rm -rf turboacc_tmp
 echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-5.10
 echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-5.10
+./scripts/feeds update -a
+./scripts/feeds install -a
