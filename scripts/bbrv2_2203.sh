e97bf4832f92fd77b31b4e7c9e49c6b43afdd71f
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-08-07 08:20:51 +0800

    Delete bbrv2_2203.sh

--- scripts/bbrv2_2203.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -rf $GITHUB_WORKSPACE/patches/BBRv2-2203/kernel/* ./target/linux/generic/hack-5.10/
-cp -rf $GITHUB_WORKSPACE/patches/BBRv2-2203/openwrt/package/kernel/linux/files/sysctl-tcp-bbr2.conf package/kernel/linux/files/sysctl-tcp-bbr2.conf
-wget -qO - https://github.com/openwrt/openwrt/commit/7db9763.patch | patch -p1
