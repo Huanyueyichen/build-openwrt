807aac5ffc93510e35f35f478e5725fb8aa98998
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2023-08-14 16:02:50 +0800

    Update diy-rockchip.sh

--- main/diy-rockchip.sh
+++ main/diy-rockchip.sh
@@ -27,6 +27,10 @@ cp -f -p $GITHUB_WORKSPACE/patches/start-rk3328-pwm-fan.sh target/linux/rockchip
 ## 添加rtl8821cu驱动
 svn export https://github.com/immortalwrt/immortalwrt/trunk/package/kernel/rtl8821cu package/kernel/rtl8821cu
 
+## 添加 luci 界面 cpu 温度，cpu 使用率
+rm -rf feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+wget https://github.com/immortalwrt/luci/raw/master/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
 # rm -rf package/new
 mkdir -p package/new
 
