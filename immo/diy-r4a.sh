a45ea9a9442bd647f405a4b3ac63e95352d3c6e1
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-02-15 21:47:06 +0800

    Update diy-r4a.sh

--- immo/diy-r4a.sh
+++ immo/diy-r4a.sh
@@ -18,8 +18,8 @@ sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 # sed -i "s/ipv6_disable 0/ipv6_disable 1/g" feeds/packages/net/miniupnpd/files/miniupnpd.init
 # cat feeds/packages/net/miniupnpd/files/miniupnpd.init
 
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 19.x feeds/packages/lang/golang
 
 #############################################################################################################
 
