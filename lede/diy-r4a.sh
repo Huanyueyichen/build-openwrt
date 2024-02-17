9ea3835f559fbe34571c93ce997a41eb1efd7dba
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-02-17 14:17:59 +0800

    Update diy-r4a.sh

--- lede/diy-r4a.sh
+++ lede/diy-r4a.sh
@@ -32,7 +32,12 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 git clone --depth 1 https://github.com/chenmozhijin/luci-app-socat package/new/socat && mv -n package/new/socat/luci-app-socat package/new/; rm -rf package/new/socat
 
 ## Add luci-app-ddns-go
-git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/new/ddnsgo && mv -n package/new/ddnsgo/*ddns-go package/new/; rm -rf package/new/ddnsgo
+git clone https://github.com/immortalwrt/packages package/new/immortalwrt-packages
+mv package/new/immortalwrt-packages/net/ddns-go package/new/ddns-go
+rm -rf package/new/immortalwrt-packages
+git clone https://github.com/immortalwrt/luci package/new/immortalwrt-luci
+mv package/new/immortalwrt-luci/applications/luci-app-ddns-go package/new/luci-app-ddns-go
+rm -rf package/new/immortalwrt-luci
 
 ## clone kiddin9/openwrt-packages仓库
 git clone https://github.com/kiddin9/openwrt-packages package/new/openwrt-packages
