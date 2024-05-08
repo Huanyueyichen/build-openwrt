a467b6e89946750338cc2e42b9c3bfa1628f0f77
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-05-08 08:19:06 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -11,8 +11,8 @@ sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 ## alist编译环境
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 
 # rm -rf package/new
