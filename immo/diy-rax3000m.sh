1b9728058a0e69097fde927e7c150fbdb2c49bbe
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-05-08 15:27:05 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -22,16 +22,16 @@ mkdir -p package/new
 # cp -rf $GITHUB_WORKSPACE/patches/default-settings package/new/default-settings
 
 ## 下载主题luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/new/luci-app-argon-config
+# rm -rf feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/applications/luci-app-argon-config
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/new/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/new/luci-app-argon-config
 ## 调整 LuCI 依赖，去除 luci-app-opkg，替换主题 bootstrap 为 argon
 # sed -i '/+luci-light/d;s/+luci-app-opkg/+luci-light/' ./feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' ./feeds/luci/collections/luci-light/Makefile
 ## 修改argon背景图片
-rm -rf package/new/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/bg1.jpg package/new/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+rm -rf feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 ## Add luci-app-alist
 # rm -rf feeds/luci/applications/luci-app-alist
