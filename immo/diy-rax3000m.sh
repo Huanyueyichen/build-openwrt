f0e594f5a9c11cae755f3029bbaf8ac56f2cbeb7
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-05-09 09:13:55 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -33,12 +33,6 @@ cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-s
 # git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 
-## Add luci-app-alist
-# rm -rf feeds/luci/applications/luci-app-alist
-# rm -rf feeds/packages/net/alist
-# git clone -b openwrt-23.05 https://github.com/immortalwrt/packages package/new/immortalwrt-packages
-# mv package/new/immortalwrt-packages/net/alist package/new/alist
-
 # mv package/new/immortalwrt-packages/lang/golang feeds/packages/lang/golang
 
 # rm -rf package/new/immortalwrt-packages
@@ -73,9 +67,11 @@ git clone https://github.com/kiddin9/openwrt-packages package/new/openwrt-packag
 
 ## alist编译环境
 rm -rf feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/packages/net/alist
 mv package/new/openwrt-packages/golang feeds/packages/lang/golang
-mv package/new/openwrt-packages/net/alist package/new/alist
-mv package/new/openwrt-packages/net/luci-app-alist package/new/luci-app-alist
+mv package/new/openwrt-packages/alist package/new/alist
+mv package/new/openwrt-packages/luci-app-alist package/new/luci-app-alist
 
 ## Add luci-app-wolplus
 mv package/new/openwrt-packages/luci-app-wolplus package/new/luci-app-wolplus
@@ -105,33 +101,6 @@ ls -1 package/new/
 
 ## ssr passwall vssr bypass依赖
 # git clone https://github.com/kenzok8/small package/new/small
-
-## passwall
-# rm -rf feeds/luci/applications/luci-app-passwall
-# rm -rf feeds/packages/net/brook
-# rm -rf feeds/packages/net/chinadns-ng
-# rm -rf feeds/packages/net/dns2socks
-# rm -rf feeds/packages/net/dns2tcp
-# rm -rf feeds/packages/net/hysteria
-# rm -rf feeds/packages/net/ipt2socks
-# rm -rf feeds/packages/net/microsocks
-# rm -rf feeds/packages/net/naiveproxy
-# rm -rf feeds/packages/net/pdnsd-alt
-# rm -rf feeds/packages/net/shadowsocks-rust
-# rm -rf feeds/packages/net/shadowsocksr-libev
-# rm -rf feeds/packages/net/simple-obfs
-# rm -rf feeds/packages/net/sing-box
-# rm -rf feeds/packages/net/ssocks
-# rm -rf feeds/packages/net/tcping
-# rm -rf feeds/packages/net/trojan-go
-# rm -rf feeds/packages/net/trojan-plus
-# rm -rf feeds/packages/net/trojan
-# rm -rf feeds/packages/net/tuic-client
-# rm -rf feeds/packages/net/v2ray-core
-# rm -rf feeds/packages/net/v2ray-geodata
-# rm -rf feeds/packages/net/v2ray-plugin
-# rm -rf feeds/packages/net/xray-core
-# rm -rf feeds/packages/net/xray-plugin
 
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/new/openwrt-passwall && mv -n package/new/openwrt-passwall/luci-app-passwall package/new/; rm -rf package/new/openwrt-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages package/new/passwall
