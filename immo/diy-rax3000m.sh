57767759a79963f818ac9f3d6b1a4a8264510709
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-05-10 09:42:00 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -28,7 +28,6 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/' ./feeds/luci/collections/luci-
 rm -rf feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
-
 # mv package/new/immortalwrt-packages/lang/golang feeds/packages/lang/golang
 
 # rm -rf package/new/immortalwrt-packages
@@ -66,7 +65,10 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/packages/net/alist
-git clone https://github.com/sbwml/luci-app-alist package/new/luci-app-alist
+git clone https://github.com/sbwml/luci-app-alist package/new/sbwml-alist
+mv package/new/sbwml-alist/luci-app-alist package/new/luci-app-alist
+mv package/new/sbwml-alist/alist package/new/alist
+rm -rf package/new/sbwml-alist
 # mv package/new/openwrt-packages/golang feeds/packages/lang/golang
 # mv package/new/openwrt-packages/alist package/new/alist
 # mv package/new/openwrt-packages/luci-app-alist package/new/luci-app-alist
@@ -139,4 +141,5 @@ bash $GITHUB_WORKSPACE/scripts/zsh.sh
 ## turboacc
 # bash $GITHUB_WORKSPACE/scripts/turboacc_5_15.sh
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
 ls -1 package/new/
