a71058a36d45f608c15a5267b3ca7dc8fc5f2418
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-07-12 15:20:15 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -102,8 +102,8 @@ rm -rf package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_t
 cp -rf $GITHUB_WORKSPACE/patches/AdGuardHome/AdGuardHome_template.yaml package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_template.yaml
 rm -rf package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
 cp -rf $GITHUB_WORKSPACE/patches/AdGuardHome/links.txt package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
-sed -i 's/LUCI_DEPENDS:=+!wget&&!curl&&!wget-ssl:curl +adguardhome/LUCI_DEPENDS:=+!wget&&!curl&&!wget-ssl:curl/g' package/new/luci-app-adguardhome/Makefile
-cat package/new/luci-app-adguardhome/Makefile
+rm -rf package/new/luci-app-adguardhome/Makefile
+cp -rf $GITHUB_WORKSPACE/patches/AdGuardHome/Makefile package/new/luci-app-adguardhome/Makefile
 # sed -i '1,2d' package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
 # sed -i 's/6h/1h/g' package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_template.yaml
 # sed -i 's/4194304/8388608/g' package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_template.yaml
