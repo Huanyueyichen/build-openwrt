0f794f381cfd0358718ec5fde4f678ca130d4212
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-08-18 12:10:22 +0800

    Update start-rk3328-pwm-fan.sh

--- patches/start-rk3328-pwm-fan.sh
+++ patches/start-rk3328-pwm-fan.sh
@@ -17,18 +17,18 @@ ISENABLE=`cat /sys/class/pwm/pwmchip0/pwm0/enable`
 if [ $ISENABLE -eq 1 ]; then
     echo -n 0 > /sys/class/pwm/pwmchip0/pwm0/enable
 fi
-echo -n 50000 > /sys/class/pwm/pwmchip0/pwm0/period
+echo -n 60000 > /sys/class/pwm/pwmchip0/pwm0/period
 echo -n 1 > /sys/class/pwm/pwmchip0/pwm0/enable
 
-echo -n 49990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+echo -n 59990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 
 while true
 do
         temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-        if  [ $temp -gt 50000 ]; then
-                echo 30000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
-        elif  [ $temp -le 40000 ]; then
-                echo 49990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
+        if  [ $temp -gt 60000 ]; then
+                echo 40000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
+        elif  [ $temp -le 50000 ]; then
+                echo 59990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
         fi
         sleep 60s;
 done
