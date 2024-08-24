19a230e744c657d515871a6ddcf1b4cf66f6063c
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-08-24 21:21:43 +0800

    Update diy-rockchip-main.sh

--- main/diy-rockchip-main.sh
+++ main/diy-rockchip-main.sh
@@ -7,7 +7,7 @@
 sed -i 's,SNAPSHOT,,g' include/version.mk
 # sed -i 's,snapshots,,g' include/version.mk
 sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
-sed -i 's,snapshots,,g' package/base-files/image-config.in
+# sed -i 's,snapshots,,g' package/base-files/image-config.in
 
 ## 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
