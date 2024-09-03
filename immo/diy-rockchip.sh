41948f60a896d3fc089e8dbdc3f1f14ba5a66f8c
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-09-03 10:11:07 +0800

    Update diy-rockchip.sh

--- immo/diy-rockchip.sh
+++ immo/diy-rockchip.sh
@@ -65,14 +65,14 @@ git clone https://github.com/kiddin9/openwrt-packages package/new/openwrt-packag
 ########## 添加包
 
 ## alist编译环境
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/net/alist
-git clone https://github.com/sbwml/luci-app-alist package/new/sbwml-alist
-mv package/new/sbwml-alist/luci-app-alist package/new/luci-app-alist
-mv package/new/sbwml-alist/alist package/new/alist
-rm -rf package/new/sbwml-alist
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+# rm -rf feeds/luci/applications/luci-app-alist
+# rm -rf feeds/packages/net/alist
+# git clone https://github.com/sbwml/luci-app-alist package/new/sbwml-alist
+# mv package/new/sbwml-alist/luci-app-alist package/new/luci-app-alist
+# mv package/new/sbwml-alist/alist package/new/alist
+# rm -rf package/new/sbwml-alist
 
 
 ## Add luci-app-wolplus
