4663c6bbfafa6272d0805ad03ebdc4e19e5a7ed7
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2024-09-06 14:31:43 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -49,9 +49,9 @@ mv -n package/new/ddnsgo/*ddns-go package/new/
 rm -rf package/new/ddnsgo
 
 ## Add luci-app-mosdns
-# rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/new/luci-app-mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/new/v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/new/luci-app-mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/new/v2ray-geodata
 
 
 ## clone kiddin9/openwrt-packages仓库
@@ -115,11 +115,11 @@ cp -rf $GITHUB_WORKSPACE/patches/AdGuardHome/links.txt package/new/luci-app-adgu
 # mv package/new/openwrt-packages/luci-app-smartdns package/new/luci-app-smartdns
 
 ## Add luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-mv package/new/openwrt-packages/v2ray-geodata package/new/v2ray-geodata
-mv package/new/openwrt-packages/v2dat package/new/v2dat
-mv package/new/openwrt-packages/mosdns package/new/mosdns
-mv package/new/openwrt-packages/luci-app-mosdns package/new/luci-app-mosdns
+# rm -rf feeds/packages/net/v2ray-geodata
+# mv package/new/openwrt-packages/v2ray-geodata package/new/v2ray-geodata
+# mv package/new/openwrt-packages/v2dat package/new/v2dat
+# mv package/new/openwrt-packages/mosdns package/new/mosdns
+# mv package/new/openwrt-packages/luci-app-mosdns package/new/luci-app-mosdns
 
 
 rm -rf package/new/openwrt-packages
