dc8388e098aafaa983215e3b3a6e06959903af82
Huanyueyichen <44775776+Huanyueyichen@users.noreply.github.com>
2025-07-03 07:56:03 +0800

    Update diy-rax3000m.sh

--- immo/diy-rax3000m.sh
+++ immo/diy-rax3000m.sh
@@ -28,7 +28,7 @@ cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-s
 
 ## golang编译环境
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 ## Add luci-app-ddns-go
 rm -rf feeds/luci/applications/luci-app-ddns-go
